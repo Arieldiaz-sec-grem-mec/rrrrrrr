0d22d6c5c69bf1603a353609c9f2d1b06cacd37b
Arieldiaz-sec-grem-mec <sec.grem.rama.mecanicos@gmail.com>
2025-03-28 14:39:23 -0300

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,12 +5,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Configuración de Firebase (Reemplaza con tu configuración)
 const firebaseConfig = {
-    apiKey: "AIzaSyCXEwoa0IIHcxu5AYbNyb7jwT-b3z2quOM",
-    authDomain: "react-modelo.firebaseapp.com",
-    projectId: "react-modelo",
-    storageBucket: "react-modelo.firebasestorage.app",
-    messagingSenderId: "69578220102",
-    appId: "1:69578220102:web:bf43bd6a36b80cbfe66047"
+    
   };
 
 // Inicializar Firebase
